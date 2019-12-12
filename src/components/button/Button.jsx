db73572bc33a205101967dab837c412b14c09f56
Ian Greulich <ian@greulich.me>
2019-12-12 16:54:55 -0500

    update button component

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 
 import './button.less';
 
-/**
- * General component description in JSDoc format, from the source code. Markdown **is** supported.
- */
 const Button = ({ label, handleClick }) => <button className="button" onClick={handleClick} type="button">{label}</button>;
 
 Button.propTypes = {
-  /** Description of prop `label`. */
+  /**
+   * Description of prop `label`. This text is from the source code.
+   */
   label: PropTypes.string.isRequired,
-  /** The function you want to execute on click. */
+  /**
+   * Description of prop `handleClick`. This text is from the source code.
+   */
   handleClick: PropTypes.func.isRequired,
 };
 
--- src/components/button/Button.md
+++ src/components/button/Button.md
@@ -1,7 +1,9 @@
-General component description in a markdown file.
+This is information about our button component.
 
 ```jsx
 import Button from "./Button";
 
 <Button handleClick={() => alert('I was clicked!')} label="We Salute You!" />
 ```
+
+> This text comes from a markdown file in the `/src/components/[component]` dir.
