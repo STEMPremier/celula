72eeeac73f5a55b3b57d0c19ddafdce76d0beb4c
Ian Greulich <ian@greulich.me>
2019-12-12 16:54:16 -0500

    configure sections for styleguide

---
+++ src/styleguide/content/components.md
@@ -0,0 +1,3 @@
+This is information about our components. This text is just normal body text.
+
+> This text comes from a markdown file in the `/styleguide/content` dir.
--- styleguide.config.js
+++ styleguide.config.js
@@ -6,4 +6,21 @@ module.exports = {
     '**/*.d.ts',
     '**/src/components/**/index.{js, jsx, ts, tsx}',
   ],
+  sections: [
+    {
+      name: 'Typography',
+      description: 'This is a word about our Typography.',
+      content: 'src/styleguide/content/typography.md',
+    },
+    {
+      name: 'Colors',
+      description: 'This is a description about our color pallette.',
+      content: 'src/styleguide/content/colors.md',
+    },
+    {
+      name: 'Components',
+      content: 'src/styleguide/content/components.md',
+      components: path.join(__dirname, 'src/components/**/*.jsx'),
+    },
+  ],
 };
