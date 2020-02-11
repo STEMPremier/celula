fb4f0ff44552463f148c5c1ea6abe503e86c9aef
Jenny Schutzman <Jschutzman@stempremier.com>
2020-02-11 16:19:32 -0500

    drafted all button types in the md  and styles in the less, created color variables in palettes, added disable to propTypes in the button.jsx

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,8 +5,13 @@ import cx from 'classnames';
 import './button.less';
 
 const COLORS = [
+<<<<<<< HEAD
   'primary',
   'secondary',
+=======
+  'primary', // ask Ferg
+  'secondary', // ask Ferg
+>>>>>>> 88c86ff... drafted all button types in the md  and styles in the less, created color variables in palettes, added disable to propTypes in the button.jsx
   'blue',
   'red',
   'purple',
@@ -16,8 +21,11 @@ const COLORS = [
 
 const SIZE = ['small', 'large', 'jumbo'];
 
+<<<<<<< HEAD
 const TYPE = ['text', 'outline', 'solid'];
 
+=======
+>>>>>>> 88c86ff... drafted all button types in the md  and styles in the less, created color variables in palettes, added disable to propTypes in the button.jsx
 const Button = props => {
   const {
     children,
@@ -36,7 +44,11 @@ const Button = props => {
     {
       [`ce-button--${color}`]: COLORS.includes(color.toString().toLowerCase()),
       [`ce-button--${size}`]: SIZE.includes(size.toString().toLowerCase()),
+<<<<<<< HEAD
       [`ce-button--${type}`]: TYPE.includes(type.toString().toLowerCase()),
+=======
+      [`ce-button--${type}`]: type, // same array control format
+>>>>>>> 88c86ff... drafted all button types in the md  and styles in the less, created color variables in palettes, added disable to propTypes in the button.jsx
       'ce-button--outline': outline,
       'ce-button--disabled': disabled,
     },
@@ -90,7 +102,11 @@ Button.propTypes = {
   /**
    * Which type of button to render.
    */
+<<<<<<< HEAD
   type: PropTypes.oneOf(TYPE),
+=======
+  type: PropTypes.oneOf(['text', 'icon']),
+>>>>>>> 88c86ff... drafted all button types in the md  and styles in the less, created color variables in palettes, added disable to propTypes in the button.jsx
 };
 
 Button.defaultProps = {
