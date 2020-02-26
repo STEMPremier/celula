86a3f5ae1cd9312f0bb7c835d0bbf675c3d76277
Ian Greulich <ian@greulich.me>
2020-02-26 12:22:11 -0500

    wip jest

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,31 @@
+const path = require('path');
+
 const ceConfig = require('../webpack.config.js');
 
 module.exports = {
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
-  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+  stories: ['../src/**/**/*.stories.(jsx|mdx)'],
   webpackFinal: async config => {
     return {
       ...config,
       module: {
         ...config.module,
-        rules: ceConfig.module.rules,
+        rules: [
+          ...config.module.rules,
+          ...ceConfig.module.rules,
+        ],
       },
     };
   },
