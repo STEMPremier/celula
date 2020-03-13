5bfdb580d419bdc163fc8515bf17f709652d2c84
Ian Greulich <ian@greulich.me>
2020-03-13 09:37:05 -0400

    add support for coloring icons

--- src/components/icon/SystemIcon.jsx
+++ src/components/icon/SystemIcon.jsx
@@ -6,24 +6,44 @@ import './icon.less';
 
 import { SYSTEM_ICONS } from './types';
 
+const COLORS = [
+  'primary',
+  'secondary',
+  'black',
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'white',
+];
+
 /**
  * Icons are for sprucing up the joint.
  */
 const SystemIcon = props => {
-  const { name } = props;
+  const { color, name } = props;
 
   return (
-    <svg className="ce-icon ce-icon--system">
+    <svg className={`ce-icon ce-icon--system ce-icon--${color}`}>
       <use xlinkHref={`#icons_${name}`} />
     </svg>
   );
 };
 
 SystemIcon.propTypes = {
+  /*
+   * The color of the `<SystemIcon />`
+   */
+  color: PropTypes.oneOf(COLORS),
   /**
    * The `<SystemIcon />`.
    */
   name: PropTypes.oneOf(SYSTEM_ICONS).isRequired,
+};
+
+SystemIcon.defaultProps = {
+  color: 'black',
 };
 
 export default SystemIcon;
