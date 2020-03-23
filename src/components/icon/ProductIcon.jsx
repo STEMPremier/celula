7534f7fea09576274c8d4a53aadf719b69a5c0d8
Ian Greulich <ian@greulich.me>
2020-03-23 16:22:07 -0400

    build the product icons

--- src/components/icon/ProductIcon.jsx
+++ src/components/icon/ProductIcon.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
-import './product-icons/icons.svg';
+import './icons/icons.svg';
 import './icon.less';
 
 import { PRODUCT_ICONS } from './types';
+
+const SIZES = ['small', 'large', 'jumbo'];
 
 /** Bold icons are used with a gradient background circle.
  * These icons can be actionable or used as a static element.
  */
 const ProductIcon = props => {
-  const { name } = props;
+  const { name, size } = props;
+
+  /* eslint-disable prettier/prettier */
+  const classes = cx(
+    'ce-icon',
+    'ce-icon__product',
+    {
+      [`ce-icon--${size}`]: SIZES.includes(size.toString().toLowerCase()),
+    },
+  );
+  /* eslint-enable prettier/prettier */
 
   return (
-    <svg className="ce-icon ce-icon--product ce-icon-black">
-      <use xlinkHref={`#icons_${name}`} />
-    </svg>
+    <span className={classes}>
+      <svg className="ce-icon--white">
+        <use xlinkHref={`#icons_product-${name}`} />
+      </svg>
+    </span>
   );
 };
 
@@ -24,6 +39,14 @@ ProductIcon.propTypes = {
    * The `<ProductIcon />`
    */
   name: PropTypes.oneOf(PRODUCT_ICONS).isRequired,
+  /**
+   * The size you want the `<ProductIcon />` to be.
+   */
+  size: PropTypes.oneOf(SIZES),
+};
+
+ProductIcon.defaultProps = {
+  size: 'small',
 };
 
 export default ProductIcon;
