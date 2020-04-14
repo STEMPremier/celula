e96872ccbce78ec746df0ef6ce09a96edb6be4bc
Jenny Schutzman <Jschutzman@stempremier.com>
2020-04-14 08:08:50 -0400

    drafted select jsx and mdx file.

---
+++ src/components/form-controls/selects/Select.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable no-console */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+class Select extends React.Component {
+  handleChange = () => console.log('this.props', this.props);
+
+  render() {
+    const {
+      className,
+      disabled,
+      error,
+      name,
+      form,
+      handleChange,
+      label,
+      value,
+      options,
+    } = this.props;
+
+    const id = `${name}_${value}`;
+
+    const classes = cx(
+      'ce-select',
+      {
+        'ce-select--disabled': disabled,
+        'ce-select--error': error,
+      },
+      className,
+    );
+    return (
+      <div className={classes}>
+        <label className="ce-select--label" htmlFor={id}>
+          {label}
+        </label>
+        <select name={name} id={id} form={form}>
+          <option value={value} handleChange={handleChange}>
+            {options}
+          </option>
+        </select>
+        <span error={error} />
+      </div>
+    );
+  }
+}
+
+Select.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  form: PropTypes.string,
+  name: PropTypes.string,
+  handleChange: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  // eslint-disable-next-line react/forbid-prop-types
+  options: PropTypes.array.isRequired,
+};
+
+Select.defaultProps = {
+  name: '',
+  className: '',
+  disabled: false,
+  form: '',
+  error: '',
+};
+
+export default Select;
