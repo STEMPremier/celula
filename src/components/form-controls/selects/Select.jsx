e2ca942d85592060df98db5c184a01db9c4c09ef
Jenny Schutzman <Jschutzman@stempremier.com>
2020-04-16 11:20:04 -0400

    drafted propType descriptions. added carrotDown state and handling function.  all in jsx

--- src/components/form-controls/selects/Select.jsx
+++ src/components/form-controls/selects/Select.jsx
@@ -1,17 +1,25 @@
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable no-console */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 class Select extends React.Component {
-  handleChange = () => console.log('this.props', this.props);
+  state = {
+    showLabel: false,
+    carrotDown: false,
+  };
+
+  handleCarrot = () => {
+    this.setState(prevState => ({
+      carrotDown: !prevState.carrotDown,
+    }));
+    // eslint-disable-next-line react/destructuring-assignment
+    console.log('handleCarrot', this.state.carrotDown);
+  };
 
   render() {
     const {
       className,
-      disabled,
+      // disabled,
       error,
       name,
       form,
@@ -21,29 +29,38 @@ class Select extends React.Component {
       options,
     } = this.props;
 
-    const id = `${name}`;
+    const { showLabel, carrotDown } = this.state;
+
+    console.log(carrotDown);
+
+    const id = `${name}_${value}`;
 
     const classes = cx(
       'ce-select',
       {
-        'ce-select--disabled': disabled,
-        'ce-select--error': error,
+        // 'ce-select--disabled': disabled,
+        // 'ce-select--error': error,
       },
       className,
     );
     return (
       <div className={classes}>
-        <label className="ce-select--label" htmlFor={id}>
-          {label}
-        </label>
-        <select name={name} id={id} form={form}>
-          <option>{value}</option>
+        {showLabel && <label htmlFor={id}>{label}</label>}
+        <select
+          className="ce-select--button"
+          name={name}
+          id={id}
+          form={form}
+          onClick={() => this.handleCarrot()}
+          onChange={handleChange}
+        >
+          {value ? <option>{value}</option> : <option>{label}</option>}
           {options.map(item => (
-            <option key={item.value} handleChange={handleChange}>
-              {item.name}
-            </option>
+            <option key={item.value}>{item.name}</option>
           ))}
+          <span>practice arrow down state change</span>
         </select>
+        <span className="ce-select--outside-arrow">span 1</span>
         <span error={error} />
       </div>
     );
@@ -51,14 +68,41 @@ class Select extends React.Component {
 }
 
 Select.propTypes = {
+  /**
+   * A class name added to the select.
+   */
   className: PropTypes.string,
-  disabled: PropTypes.bool,
+  /**
+   * Make the selected disabled
+   */
+  // disabled: PropTypes.bool,
+  /**
+   * The label is required for accessibilty eventhough it will not show if you give it a default value instead.
+   */
   label: PropTypes.string.isRequired,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  /**
+   * The value is the optional prelected default option to appear in the select.
+   */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  /**
+   * The form is what the select belongs to.  NOT SURE EXACTLY.  DOUBLE CHECK WITH IAN.
+   */
   form: PropTypes.string,
+  /**
+   * NOT SURE ABOUT THIS EITHER
+   */
   name: PropTypes.string,
+  /**
+   * A function that is called when changing the `<Select />`.
+   */
   handleChange: PropTypes.func.isRequired,
+  /**
+   * The error message.
+   */
   error: PropTypes.string,
+  /**
+   * The options is the array of objects containing the name and value of each select row.
+   */
   // eslint-disable-next-line react/forbid-prop-types
   options: PropTypes.array.isRequired,
 };
@@ -66,9 +110,10 @@ Select.propTypes = {
 Select.defaultProps = {
   name: '',
   className: '',
-  disabled: false,
+  // disabled: false,
   form: '',
   error: '',
+  value: '',
 };
 
 export default Select;
