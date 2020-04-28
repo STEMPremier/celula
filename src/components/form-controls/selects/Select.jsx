1e0dd09c3c8775f86f6b23b3be8c5412b1b31e70
Jenny Schutzman <Jschutzman@stempremier.com>
2020-04-28 10:30:29 -0400

    started new background div to design on state changes

--- src/components/form-controls/selects/Select.jsx
+++ src/components/form-controls/selects/Select.jsx
@@ -86,6 +86,7 @@ class Select extends React.Component {
     return (
       <div className={classes}>
         {showLabel && <label htmlFor={id}>{label}</label>}
+
         <div className="ce-select--wrapper">
           <select
             name={name}
@@ -131,6 +132,7 @@ class Select extends React.Component {
             </button>
           )}
         </div>
+        <div className="ce-select--background-changes" />
         <span error={error} />
       </div>
     );
