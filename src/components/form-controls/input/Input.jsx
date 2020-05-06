be5519b7f984069a40c67f397daac81aa5081d9d
Jenny Schutzman <Jschutzman@stempremier.com>
2020-05-06 10:35:10 -0400

    began to draft PropTypes and html.  nothing running yet

---
+++ src/components/form-controls/input/Input.jsx
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import './input.less';
+
+const TYPES = [
+  'color',
+  'datetime-local',
+  'email',
+  'file',
+  'hidden',
+  'month',
+  'number',
+  'password',
+  'search',
+  'tel',
+  'time',
+  'url',
+  'week',
+];
+
+const ICON = ['get icons to plus in here?????'];
+
+const SIZE = ['small', 'large', 'jumbo'];
+
+class Input extends React.Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = event =>
+    this.setState({
+      value: [event.target.value],
+    });
+
+  render() {
+    const {
+      className,
+      disabled,
+      error,
+      htmlType,
+      icon,
+      label,
+      size,
+      tooltip,
+      validators,
+      value,
+    } = this.props;
+
+    return (
+      <div>
+        <label label={label} />
+
+        <input
+          className={classes}
+          type={htmlType}
+          value={value}
+          toolTip={toolTip}
+          onChange={this.handleChange}
+          icon={icon}
+          disabled={disabled}
+        />
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  htmlType: PropTypes.string,
+  icon: PropTypes.string,
+  label: PropTypes.string,
+  required,
+  size: PropTypes.string,
+};
