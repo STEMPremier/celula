604693facd3bee074a000d2e701c2506f411f4a3
Ian Greulich <ian@greulich.me>
2020-05-15 17:00:13 -0400

    make SystemIcons fit the small/large/jumbo scheme

--- src/components/icon/SystemIcon.jsx
+++ src/components/icon/SystemIcon.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 import * as CONSTANTS from '../../utils/constants';
 
 import './icons/icons.svg';
 import './icon.less';
 
-const { COLORS, SYSTEM_ICONS } = CONSTANTS;
+const { COLORS, SIZES, SYSTEM_ICONS } = CONSTANTS;
 
 /**
  * System icons are designed to be simple, modern and friendly.
  * Each icon is reduced to its minimal form, expressing essential characteristics within the interface.
  */
 const SystemIcon = props => {
-  const { color, name } = props;
+  const { color, name, size } = props;
+
+  /* eslint-disable prettier/prettier */
+  const classes = cx(
+    'ce-icon',
+    'ce-icon__system',
+    {
+      [`ce-icon--${size}`]: SIZES.includes(size.toString().toLowerCase()),
+    },
+  );
+  /* eslint-enable prettier/prettier */
 
   return (
-    <span className="ce-icon ce-icon__system">
+    <span className={classes}>
       <svg className={`ce-icon--${color}`}>
         <use xlinkHref={`#icons_system-${name}`} />
       </svg>
@@ -33,10 +44,15 @@ SystemIcon.propTypes = {
    * The `<SystemIcon />`.
    */
   name: PropTypes.oneOf(SYSTEM_ICONS).isRequired,
+  /**
+   * The size you want the `<SystemIcon />` to be.
+   */
+  size: PropTypes.oneOf(SIZES),
 };
 
 SystemIcon.defaultProps = {
   color: 'black',
+  size: 'large',
 };
 
 export default SystemIcon;
