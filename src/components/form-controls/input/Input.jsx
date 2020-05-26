71810066169837eed480ebb760867ffbb6ef5512
Jenny Schutzman <Jschutzman@stempremier.com>
2020-05-26 09:30:21 -0400

    drafted input propType descriptions.  Started the icon mdx and jsx, but not yet finished

--- src/components/form-controls/input/Input.jsx
+++ src/components/form-controls/input/Input.jsx
@@ -177,6 +177,9 @@ class Input extends Component {
           {label}
         </label>
         <div className="ce-input--box">
+          <div className="ce-input--icon">
+            <SystemIcon name={icon} />
+          </div>
           <input
             name={name}
             type={htmlType}
@@ -190,6 +193,7 @@ class Input extends Component {
             min={min}
             max={max}
           />
+
           {showArrow && (
             <button
               className="ce-input--outside-arrow"
@@ -222,30 +226,66 @@ class Input extends Component {
 }
 
 Input.propTypes = {
+  /**
+   * A class name added to the `<Input />`.
+   */
   className: PropTypes.string,
+  /**
+   * Make the `<Input />` inactive.
+   */
   disabled: PropTypes.bool,
+  /**
+   * Make the `<Input />` display in error state.
+   */
   error: PropTypes.string,
+  /**
+   * Make an icon appear in the `<Input />`.
+   */
   icon: PropTypes.string,
+  /**
+   * The `<Input />` label.
+   */
   label: PropTypes.string.isRequired,
+  /**
+   * The size of the `<Input />`.  Options include 'small', 'large', and 'jumbo'.
+   */
   size: PropTypes.string,
-  toolTip: PropTypes.string,
+  // toolTip: PropTypes.string,
+  /**
+   * The error message that will appear in the error box below the input.
+   */
   errorMessage: PropTypes.string,
+  /**
+   *  The earliest date to accept as a valid input.  Note that the date and time format will vary depending on the type of input and the min must match that format.
+   */
   min: PropTypes.string,
+  /**
+   *  The latest date to accept as a valid input.  Note that the date and time format will vary depending on the type of input and the max must match that format.
+   */
   max: PropTypes.string,
   // validators:
+  /**
+   * The value of the `<Input />`.
+   */
   value: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
     PropTypes.bool,
   ]).isRequired,
+  /**
+   * The type of the `<Input />`.  Commonly used types include text, email, url, hidden, month, week, datetime-local, time, file, password, search, tel, and color.
+   */
   htmlType: PropTypes.oneOf([...TYPES, 'default']),
+  /**
+   * The name of the `<Input />`.
+   */
   name: PropTypes.string.isRequired,
   /**
    * This function is accesible to the user for clickable events on the right arrow in the gradient
    */
   rightArrowClick: PropTypes.func,
   /**
-   * The input will appear with the right arrow
+   * This will make the right arrow button appear with the `<Input />`.
    */
   showArrow: PropTypes.bool,
 };
