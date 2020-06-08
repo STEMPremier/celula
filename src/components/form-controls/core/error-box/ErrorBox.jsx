6d8f6211906f3c7c0b8918702e7dbe96f38788c4
Ian Greulich <ian@greulich.me>
2020-06-08 12:29:58 -0400

    add base errorbox component

--- src/components/form-controls/core/error-box/ErrorBox.jsx
+++ src/components/form-controls/core/error-box/ErrorBox.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ErrorBox = () => <span>Error</span>;
+const ErrorBox = ({ errorMsg = '' }) => (
+  <div className="ce-errorbox--error__container">
+    <div>
+      <div className="ce-errorbox--error__arrow" />
+      <div className="ce-errorbox--error__message">
+        <span className="ce-errorbox--error__text">{errorMsg}</span>
+      </div>
+    </div>
+  </div>
+);
 
 export default ErrorBox;
+
+ErrorBox.propTypes = {
+  /**
+   * An error message to display
+   */
+  errorMsg: PropTypes.string.isRequired,
+};
