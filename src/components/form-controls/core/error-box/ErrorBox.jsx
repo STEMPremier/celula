4d47f8b88d044e2cf0c458f1989e2c2f0e6f2c89
Ian Greulich <ian@greulich.me>
2020-06-09 16:19:51 -0400

    fix the weird naming scheme i used

--- src/components/form-controls/core/error-box/ErrorBox.jsx
+++ src/components/form-controls/core/error-box/ErrorBox.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 import './error-box.less';
 
 const ErrorBox = ({ errorMsg = '' }) => (
-  <div className="ce-errorbox--error__container">
-    <div>
-      <div className="ce-errorbox--error__arrow" />
-      <div className="ce-errorbox--error__message">
-        <span className="ce-errorbox--error__text">{errorMsg}</span>
+  <div className="ce-errorbox">
+    <div className="ce-errorbox__container">
+      <div className="ce-errorbox__arrow" />
+      <div className="ce-errorbox__message">
+        <span className="ce-errorbox__text">{errorMsg}</span>
       </div>
     </div>
   </div>
