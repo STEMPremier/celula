bda58c36ad7592b4c0cbd7d4e61e5b4b2f154dbf
Ian Greulich <ian@greulich.me>
2020-06-26 16:53:47 -0400

    refactor FormControlGroup to use react hooks

--- src/components/form-controls/core/form-control-group/FormControlGroup.jsx
+++ src/components/form-controls/core/form-control-group/FormControlGroup.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 /**
@@ -8,36 +8,48 @@ import PropTypes from 'prop-types';
  *
  * `FormControlGroup` has no specific styles beyone those it children componets bring with them.
  */
-class FormControlGroup extends Component {
-  state = {
-    selectedValues: this.props.selectedValues, // eslint-disable-line react/destructuring-assignment
-  };
+const FormControlGroup = props => {
+  const {
+    className,
+    disabled,
+    errorClass,
+    errorMsg,
+    formId,
+    label,
+    name,
+    selectedValues,
+  } = props;
+  const [values, setValues] = useState(selectedValues);
 
-  handleCheckboxChange = event => {
-    const { handleChange } = this.props;
-    let { selectedValues } = this.state;
+  useEffect(() => {
+    setValues(values);
+  }, [selectedValues]);
 
-    if (event.target.checked) {
-      selectedValues.push(event.target.value);
+  const handleCheckboxChange = event => {
+    const { handleChange } = props;
+    const { checked, value } = event.target;
+    let selectedVals = [...values];
+
+    if (checked) {
+      selectedVals.push(value);
     } else {
-      selectedValues = selectedValues.filter(val => val !== event.target.value);
+      selectedVals = selectedVals.filter(val => val !== value);
     }
 
-    handleChange(event.target.value);
-
-    this.setState({ selectedValues });
+    handleChange(value);
+    setValues(values);
   };
 
-  handleRadioChange = event => {
-    const { handleChange } = this.props;
+  const handleRadioChange = event => {
+    const { handleChange } = props;
+    const { value } = event.target;
 
-    handleChange(event.target.value);
-
-    this.setState({ selectedValues: [event.target.value] });
+    handleChange(value);
+    setValues([values]);
   };
 
-  handleChange = event => {
-    let { children } = this.props;
+  const handleChange = event => {
+    let { children } = props;
     let childType;
 
     children = React.Children.toArray(children);
@@ -48,42 +60,37 @@ class FormControlGroup extends Component {
     }
 
     // there are only 2 types of children, ever
-    if (childType === 'radio') this.handleRadioChange(event);
-    if (childType === 'checkbox') this.handleCheckboxChange(event);
+    if (childType === 'radio') handleRadioChange(event);
+    if (childType === 'checkbox') handleCheckboxChange(event);
   };
 
-  renderChildren = () => {
-    const { childProps, children, name } = this.props;
-    const { selectedValues } = this.state;
+  const renderChildren = () => {
+    const { children, childProps } = props;
 
     return React.Children.map(children, child => {
-      const props = {
-        checked: selectedValues.includes(child.props.value),
+      const newProps = {
+        checked: values.includes(child.props.value),
         name,
         ...childProps,
       };
 
-      return React.cloneElement(child, { ...props });
+      return React.cloneElement(child, { ...newProps });
     });
   };
 
-  render() {
-    const { className, disabled, errorClass, errorMsg, formId, label } = this.props;
-
-    return (
-      <fieldset
-        className={className}
-        disabled={disabled}
-        form={formId}
-        onChange={this.handleChange}
-      >
-        <legend>{label}</legend>
-        {this.renderChildren()}
-        <span className={errorClass}>{errorMsg}</span>
-      </fieldset>
-    );
-  }
-}
+  return (
+    <fieldset
+      className={className}
+      disabled={disabled}
+      form={formId}
+      onChange={handleChange}
+    >
+      <legend>{label}</legend>
+      {renderChildren()}
+      <span className={errorClass}>{errorMsg}</span>
+    </fieldset>
+  );
+};
 
 FormControlGroup.propTypes = {
   /**
