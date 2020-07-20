69fa4c9367996ec0314435604ba025c99e61661c
Ian Greulich <ian@greulich.me>
2020-07-20 12:20:24 -0400

    fix some poor export choices, and add some that we completely forgot

--- src/components/form-controls/index.js
+++ src/components/form-controls/index.js
@@ -1,5 +1,18 @@
-import { FormControlGroup } from './core';
-import * as Radio from './radio';
+import { Checkbox, CheckboxGroup } from './checkbox';
+import { ErrorBox, FormControlGroup } from './core';
+import Input from './input';
+import { Radio, RadioGroup } from './radio';
+import Select from './select';
 import SubmitButton from './submit-button';
 
-export { FormControlGroup, Radio, SubmitButton };
+export {
+  Checkbox,
+  CheckboxGroup,
+  ErrorBox,
+  FormControlGroup,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+  SubmitButton,
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import Button from './components/button';
-import * as FormControls from './components/form-controls';
-import * as Icon from './components/icon';
-import * as ToggleButton from './components/toggle-button';
+import {
+  Checkbox,
+  CheckboxGroup,
+  ErrorBox,
+  FormControlGroup,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+  SubmitButton,
+} from './components/form-controls';
+import { ProductIcon, SecondaryIcon, SystemIcon } from './components/icon';
+import { ToggleButton, ToggleButtonGroup } from './components/toggle-button';
 
 import './styles/celula.less';
 
-export { Button, FormControls, Icon, ToggleButton };
+export {
+  Button,
+  Checkbox,
+  CheckboxGroup,
+  ErrorBox,
+  FormControlGroup,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+  SubmitButton,
+  ProductIcon,
+  SecondaryIcon,
+  SystemIcon,
+  ToggleButton,
+  ToggleButtonGroup,
+};
