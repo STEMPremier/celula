6fa7ea64e344fb234ea44f85f6872d759fe8b265
Ian Greulich <ian@greulich.me>
2020-08-11 13:59:56 -0400

    add Container stories and specs

---
+++ src/components/container/Container.spec.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import Container from '.';
+
+describe('<Container />', () => {
+  describe('render()', () => {
+    test('render the Container component', () => {
+      const wrapper = shallow(
+        <Container>
+          <span>This is stuff in a container.</span>
+        </Container>,
+      );
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+});
+/* eslint-enable react/jsx-filename-extension */
