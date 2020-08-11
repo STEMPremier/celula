3e0cd51f619abbfb3680ffd3e06203218d3420a2
Jenny Schutzman <Jschutzman@stempremier.com>
2020-08-11 09:32:54 -0400

    drafted all story types in mdx and got fileUploader proptypes and jsx sorted up.  up and running but unstyled

--- src/components/form-controls/fileUploader/FileUploader.jsx
+++ src/components/form-controls/fileUploader/FileUploader.jsx
@@ -1,9 +1,12 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 import { ErrorBox } from '../core';
+
+import './file-uploader.less';
 
 import {
   FILEUPLOADER_TYPES as TYPES,
@@ -12,10 +15,10 @@ import {
 } from '../../../utils/constants';
 
 const FileUploader = ({
-  // accept,
+  accept,
   children,
   className,
-  // capture,
+  capture,
   color,
   disabled,
   errorMsg,
@@ -24,12 +27,12 @@ const FileUploader = ({
   helpText,
   initialValue,
   label,
-  // multiple,
+  multiple,
   name,
   size,
   styleType,
 }) => {
-  const [fieldValue, setFieldValue] = useState(initialValue);
+  const [fieldValue, setFieldValue] = useState('');
   const [errMsg, setErrMsg] = useState(errorMsg);
 
   const classes = cx(
@@ -53,10 +56,6 @@ const FileUploader = ({
   const id = `${name}`;
 
   useEffect(() => {
-    setFieldValue(initialValue);
-  }, [initialValue]);
-
-  useEffect(() => {
     setErrMsg(errorMsg);
   }, [errorMsg]);
 
@@ -68,32 +67,49 @@ const FileUploader = ({
 
   return (
     <div className={classes}>
+      <label htmlFor={id}>{children}</label>
+      {helpText && <div className="ce-fileuploader__help-text">{helpText}</div>}
       <input
-        type="file"
+        accept={accept}
+        capture={capture}
+        className={styleType}
+        color={color}
         disabled={disabled}
         form={formId}
         id={id}
+        multiple={multiple}
         name={name}
-        size={size}
-        color={color}
-        styleType={styleType}
-        // onBlur={checkValiditiy}
         onChange={handleClick}
+        size={size}
+        // onBlur={checkValiditiy}
+        type="file"
         value={fieldValue}
       />
-      <label htmlFor={id}>{children}</label>
-      {helpText && <div className="ce-fileuploader__help-text">{helpText}</div>}
-      {errMsg && <ErrorBox errorMsg={errMsg} />}
+      {errorMsg && <ErrorBox errorMsg={errorMsg} />}
     </div>
   );
 };
 
 FileUploader.propTypes = {
+  /**
+   * Defines the file types the file input should accept.  Inside the string, create a comma-separated list of unique file type specificiers.  An example would be accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document".
+   */
+  accept: PropTypes.string,
+  /**
+   * Specifies which cameria to use for capture of image or video data, if the accept attribute indicates that the inut should be one of those types.  A value of 'user' indicates that the user-facing camera and/or microphone should be used.  A value of 'enviroment' specifies that the outward-facing camera and/or microphone should be used.  If no value is declared, the use agent is free to decide on its own what to do.
+   */
+  capture: PropTypes.string,
+  /**
+   *
+   */
   children: PropTypes.string.isRequired,
   /**
    * A class name, or string of class names, to add to the `<Input />`.
    */
   className: PropTypes.string,
+  /**
+   *
+   */
   color: PropTypes.string,
   /**
    * Disables the `<Input />`.
@@ -128,14 +144,26 @@ FileUploader.propTypes = {
    */
   label: PropTypes.string.isRequired,
   /**
+   * Allow the use to select more than one file.
+   */
+  multiple: PropTypes.bool,
+  /**
    * The name given to the `<Input />`. It connects the label to the `<Input />`.
    */
   name: PropTypes.string.isRequired,
+  /**
+   *
+   */
   size: PropTypes.string,
+  /**
+   *
+   */
   styleType: PropTypes.string,
 };
 
 FileUploader.defaultProps = {
+  accept: '',
+  capture: '',
   className: '',
   color: 'primary',
   disabled: false,
@@ -144,6 +172,7 @@ FileUploader.defaultProps = {
   handleClick: () => {},
   helpText: '',
   initialValue: null,
+  multiple: false,
   size: 'large',
   styleType: 'default',
 };
