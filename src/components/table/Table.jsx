b25dace72575eb9de0c5d8c39810fa4e2dd9a664
Jenny Schutzman <Jschutzman@stempremier.com>
2020-08-31 14:39:06 -0400

    created a boolean status props that triggers a first column iwth header status and table data of status.  The three status selections are complete, in progress, and submitted.  The jxs, html, and mdx and ready.  Now I just need to create the relevant icons and swap out wording with the new styled icons

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -8,13 +8,15 @@ import cx from 'classnames';
 
 import './table.less';
 
-// SPENT HOURS TRYING TO LINE UP ROWS UNDER COLUMNS
-// BY MAPPING OVER A JOINT DATATABLE OBJECT BUT DIVS WITH MAPPING BREAK THE FORMAT
-// NOW NEED TO CREATE FUNCTIONS THAT WILL PUT THE HEADERS AND ROWS IN STATE THAT I CAN
-// ACCESS QUICKLY IN THE JSX TABLE TAGS
-// DO ALL LOGIC ABOVE THE RETURN
-
-const Table = ({ className, handleChange, headings, label, name, rows }) => {
+const Table = ({
+  className,
+  handleChange,
+  headings,
+  label,
+  name,
+  rows,
+  status,
+}) => {
   // const [headers, setHeaders] = useState();
   // const [rows, setRows] = useState();
 
@@ -26,86 +28,6 @@ const Table = ({ className, handleChange, headings, label, name, rows }) => {
     'Job Title',
     'Age',
   ];
-  // const getHeaders = headingData => {
-  //   // headingData.map(header => setHeaders(header));
-  //   console.log('headingData', headingData);
-
-  // };
-
-  // const propRows = [{
-  //   data: [{
-  //     cell1: '',
-  //     cell2: ''
-  //   }]
-  // }]
-
-  // const propRows = [
-  //   {
-  //     data: ['Jenny', 'Schutzman', 'Tallo', 'Software Engineer', '36'],
-  //     status: '',
-  //   },
-  //   {
-  //     data: ['David', 'Morrow', 'Gildan', 'Supply Chain Manager', '43'],
-  //     status: '',
-  //   },
-  //   {
-  //     data: [
-  //       'Gina',
-  //       'Nicholas',
-  //       'Proctor and Gamble',
-  //       'Evaluation Data Manager',
-  //       '34',
-  //     ],
-  //     status: '',
-  //   },
-  // ];
-  // // not sure if I should push it into something visible or map over rows in jsx
-  // const row = propRows.map(item => item.data);
-  // const rowItems = row.map(item => item);
-  // console.log('rowItems', rowItems);
-  // object.map(row => setRows(row));
-  // object.map(row => console.log('row in map', row));
-  // console.log('rows', rows);
-  // console.log('headings', headings);
-
-  // Build an object from the table props to insert into the mapped jsx tags
-  // const dataObject = {
-  //   rows: propRows.map(item => item.data),
-  //   headers: propHeadings,
-  // };
-  // // rowItems(propRows);
-  // console.log('dataObject', dataObject);
-  // getHeaders(propHeadings);
-
-  //   <tr>
-  //   <th>{dataObject.headers}</th>
-  // </tr>
-  // <tr>
-  //   {dataObject.rows.map(row => (
-  //     <td>{row}</td>
-  //   ))}
-  // </tr>
-
-  // FIRST ATTEMPT
-  // {
-  //   headers.map(header => (
-  //     <div key={header}>
-  //       <tr>
-  //         <th>{header}</th>
-  //       </tr>
-  //     </div>
-  //   ));
-  // }
-  // {
-  //   rows.map(row => (
-  //     <div key={row}>
-  //       <tr>
-  //         <td>{row.data}</td>
-  //       </tr>
-  //     </div>
-  //   ));
-  // }
-  // END FIRST ATTEMPT JSX
 
   return (
     <div>
@@ -120,6 +42,7 @@ const Table = ({ className, handleChange, headings, label, name, rows }) => {
         >
           {headings && (
             <tr>
+              {status && <th>Status</th>}
               {headings.map(header => (
                 <th>{header}</th>
               ))}
@@ -128,6 +51,7 @@ const Table = ({ className, handleChange, headings, label, name, rows }) => {
 
           {rows.map(rowItems => (
             <tr>
+              <td>{rowItems.status}</td>
               {rowItems.data.map(rowData => (
                 <td>{rowData}</td>
               ))}
@@ -168,13 +92,15 @@ Table.propTypes = {
   rows: PropTypes.arrayOf(
     PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool]),
   ),
+  status: PropTypes.bool,
 };
 
 Table.defaultProps = {
-  rows: [''],
+  className: '',
   name: '',
   handleChange: () => {},
-  className: '',
+  rows: [''],
+  status: false,
 };
 
 export default Table;
