a42aa4a8822b3d67f84802c91e49fc7aef113a77
Ian Greulich <ian@greulich.me>
2020-09-04 12:47:39 -0400

    remove comment out code

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,8 @@
 import { addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
-// import centered from '@storybook/addon-centered/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 addDecorator(withKnobs);
-// addDecorator(centered); // Wraps the components in both canvas, and docs
 
 addParameters({
   viewport: {
