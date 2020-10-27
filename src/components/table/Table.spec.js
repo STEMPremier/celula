4a9ffa18118523b6415db8d2bb9780d1c39f95de
Ian Greulich <ian@greulich.me>
2020-10-27 16:22:31 -0400

    update tests and snapshots

--- src/components/table/Table.spec.js
+++ src/components/table/Table.spec.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-import { data, columns } from './data.js';
+import { data, columns } from './data';
 import Table from '.';
 
 describe('<Table />', () => {
